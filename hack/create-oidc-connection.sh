279b49c21323e04b5b73e4c3cd6543ca07fdd120
Alex Benn <alexbenn@microsoft.com>
2026-02-13 15:34:23 -0500

    Fix OIDC connection creation script. It works now!

--- hack/create-oidc-connection.sh
+++ hack/create-oidc-connection.sh
@@ -12,8 +12,8 @@ RESOURCE_GROUP_NAME=${RESOURCE_GROUP_NAME:-alexbenn-test}
 
 az identity create --name $AZURE_IDENTITY_NAME \
     --resource-group $RESOURCE_GROUP_NAME
-SP_ID=$(az identity show --name $AZURE_IDENTITY_NAME --resource-group $RESOURCE_GROUP_NAME --query 'clientId' -o tsv)
-az role assignment create --assignee $SP_ID --role "Contributor" --scope "/subscriptions/$AZURE_SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME"
+SP_PRINCIPAL_ID=$(az identity show --name $AZURE_IDENTITY_NAME --resource-group $RESOURCE_GROUP_NAME --query 'principalId' -o tsv)
+az role assignment create --assignee-object-id $SP_PRINCIPAL_ID --assignee-principal-type ServicePrincipal --role "Contributor" --scope "/subscriptions/$AZURE_SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME"
 FIC_ID="github-azure-aks-stretch-image-builder-fic"
 # This grants access to operations run on the main branch, which seems reasonable.
 az identity federated-credential create --name $FIC_ID --identity-name $AZURE_IDENTITY_NAME --resource-group $RESOURCE_GROUP_NAME --issuer 'https://token.actions.githubusercontent.com' --subject 'repo:abenn135/azure-aks-stretch-image-builder:ref:refs/heads/main' --audiences 'api://AzureADTokenExchange'
